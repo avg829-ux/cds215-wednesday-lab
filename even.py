b604840ca29273d18a4789260681243015e388d1
avg829-ux <avg829@culver.edu>
2026-09-16 15:01:00 -0500

    Update even.py

--- even.py
+++ even.py
@@ -6,7 +6,7 @@ while count <= number*5:
     if count % 2 ==0:
         print(count, "is even")
     else:
-        print (count, "is odd")
+        print (count, "is odd number")
 
     count = count +1 
 print("Finished")
